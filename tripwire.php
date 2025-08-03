5fd17aae4b306276637e61b128249e5f83a7b379
Astriania <singularity.syndicate.eve@gmail.com>
2025-08-03 23:34:46 +0100

    Sig dialog shortcut buttons

--- tripwire.php
+++ tripwire.php
@@ -410,6 +410,7 @@ $system = $_REQUEST['system'];
 						<span data-autocomplete="sigTypeFrom">
 							<input name="wormholeType" type="text" class="wormholeType" maxlength="4" size="4" autocomplete="off" />
 						</span>
+						<span id="wormholeTypeQuickSelectFrom" class="wormholeTypeQuickSelect quickSelectBar"></span>
 						<!-- <span class="bookmark">
 							<span class="label">BM:</span>
 							<input name="" type="text" maxlength="10" size="8" />
@@ -423,6 +424,10 @@ $system = $_REQUEST['system'];
 								<!-- Values filled in by signature dialog JS -->
 							</select>
 						</span>
+						<!-- needs to not be whitespace between the buttons for alignment, even though it's ugly markup -->
+						<div id="leadsToQuickBar" class="quickSelectBar"><button type="button" class="quick-select" value="Class-1" tabindex="-1"><span class="class-1">C1<span></button><button type="button" class="quick-select" value="Class-2" tabindex="-1"><span class="class-2">C2<span></button><button type="button" class="quick-select" value="Class-3" tabindex="-1"><span class="class-3">C3<span></button><button type="button" class="quick-select" value="Class-4" tabindex="-1"><span class="class-4">C4<span></button><button type="button" class="quick-select" value="Class-5" tabindex="-1"><span class="class-5">C5<span></button><button type="button" class="quick-select" value="Class-6" tabindex="-1"><span class="class-6">C6<span></button> |
+						<button type="button" class="quick-select" value="High-Sec" tabindex="-1"><span class="highsec">HS<span></button><button type="button" class="quick-select" value="Low-Sec" tabindex="-1"><span class="lowsec">LS<span></button><button type="button" class="quick-select" value="Null-Sec" tabindex="-1"><span class="nullsec">NS<span></button>
+						</div>
 					</div>
 					<div class="row">
 						<span class="label">Name:</span>
@@ -459,6 +464,7 @@ $system = $_REQUEST['system'];
 						<span data-autocomplete="sigTypeTo">
 							<input name="wormholeType2" type="text" class="wormholeType" data-autocomplete="sigType" maxlength="4" size="4" autocomplete="off" />
 						</span>
+						<span id="wormholeTypeQuickSelectTo" class="wormholeTypeQuickSelect quickSelectBar"></span>
 						<!-- <span class="bookmark">
 							<span class="label">BM:</span>
 							<input name="" type="text" maxlength="10" size="8" />
